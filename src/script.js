b2a19040506331e8044ea0370d8ccf644d615a67
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-04-20 12:01:09 +0200

    learnt how to group objects and apply transformations to all objects

--- src/script.js
+++ src/script.js
@@ -9,20 +9,42 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y= 1
+group.rotation.z=1
+scene.add(group)
 
+const cube1= new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:"green"})
+)
+
+group.add(cube1)
+
+const cube2= new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:"red"})
+)
+cube2.position.x=2;
+group.add(cube2)
+
+const cube3= new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color:"blue"})
+)
+
+cube3.position.x=-2;
+
+group.add(cube3)
 
 // position
-mesh.position.set(0.5,-1, -1)
+// mesh.position.set(0.5,-1, -1)
 
-// scale
-mesh.scale.set(1,0.5, 2)
-// rotation
-mesh.rotateX(1)
-mesh.rotateY(0.7)
+// // scale
+// mesh.scale.set(1,0.5, 2)
+// // rotation
+// mesh.rotateX(1)
+// mesh.rotateY(0.7)
 
 
 
@@ -47,7 +69,7 @@ camera.position.z = 3
 scene.add(camera)
 
 
-camera.lookAt(mesh.position)
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
