fbc1caa27a89be7d555946f0db4f087880f6c0f5
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-04-21 22:09:02 +0200

    Learnt how to manually control perspective camera. Orbiting object based on mouse position. Focus on math to do single orbit around object

--- src/script.js
+++ src/script.js
@@ -1,8 +1,65 @@
 import * as THREE from 'three'
+import { log } from 'three/examples/jsm/nodes/Nodes.js';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 /**
  * Base
  */
+
+//Mouse move
+let mousePositionX=0;
+let mousePositionY=0;
+const buffer= 20;
+
+window.addEventListener("mousemove",(e)=>
+{
+    var e = window.event;
+
+    var posX = e.clientX;
+    var posY = e.clientY;
+    let countx=0
+    let county=0
+
+    if(posX<=sizes.width&&posY<=sizes.height){
+        
+        
+            mousePositionX=posX/(sizes.width)-0.5
+            mousePositionY=posY/(sizes.height)-0.5
+
+
+        
+        
+        
+        // if(countx<1){
+        //     // camera.position.x += mousePositionX/10
+        //     countx+= mousePositionX/10
+        //     console.log(countx)
+
+        // }
+        // else{
+        //     console.log("Stopping")
+        // }
+        // if(county<1){
+        //     // camera.position.y += -mousePositionY/10
+        //     countx+= -mousePositionY/10
+        // }
+    }
+    
+    
+        // console.log(mousePositionX);
+        // console.log(mousePositionY);
+
+
+
+})
+
+
+
+
+
+
+
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -23,12 +80,16 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
+const aspectRatio=  sizes.width/sizes.height
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+// const camera= new THREE.OrthographicCamera(-1*aspectRatio,1*aspectRatio,1,-1,0.1,100)
+
+
+
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -44,7 +105,18 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+    camera.position.x = 2*Math.sin(-mousePositionX*Math.PI*2)
+    camera.position.z = 2*Math.cos(mousePositionX*Math.PI*2)
+    camera.position.y = mousePositionY*3
+
+    // camera.rotation.y= Math.PI*Math.sin(mousePositionX)
+    // camera.position.x += 0.1
+
+    camera.lookAt(mesh.position)   
+    
+
+    
 
     // Render
     renderer.render(scene, camera)
