a5199ec4b80f85a517f3bbce1bf39ec709d0a52b
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-04-22 21:13:53 +0200

    created a new basic geometry using a buffer geometry object and floatarray

--- src/script.js
+++ src/script.js
@@ -11,8 +11,19 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const positionsArray= new Float32Array([
+    0,0,0,
+    0,1,0,
+    1,0,0,
+
+])
+const positionAttribute= new THREE.BufferAttribute(positionsArray,3)
+const geometry= new THREE.BufferGeometry()
+geometry.setAttribute("position",positionAttribute)
+
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe:true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
