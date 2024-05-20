f59462c2a208c18620fdbeedab12b3d36ca5096a
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-05-20 10:19:05 +0200

    Learnt basics of raycaster class. how to position and use callbacks

--- Lesson 22 Raycaster and Mouse Events/src/script.js
+++ Lesson 22 Raycaster and Mouse Events/src/script.js
@@ -36,6 +36,31 @@ object3.position.x = 2
 
 scene.add(object1, object2, object3)
 
+
+/**
+ * Raycaster
+ */
+
+object1.updateMatrixWorld()
+object2.updateMatrixWorld()
+object3.updateMatrixWorld()
+
+const raycaster= new THREE.Raycaster()
+//origin
+const rayOrigin= new THREE.Vector3(-3,0,0)
+
+//normalized direction
+const rayDirection= new THREE.Vector3(10,0,)
+rayDirection.normalize()
+//cast ray origin and direction
+raycaster.set(rayOrigin,rayDirection)
+const objectsToTest=[object1,object2,object3]
+
+const findIntersectingObjects= (objectsToTest)=>{
+    return raycaster.intersectObjects(objectsToTest)
+     
+}
+
 /**
  * Sizes
  */
@@ -89,6 +114,24 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    //animate objects
+    object1.position.y=Math.sin(elapsedTime*0.3)
+
+    object2.position.y=Math.sin(elapsedTime*0.8)
+
+
+    object3.position.y=Math.sin(elapsedTime*0.5)
+
+    const intersectingObjects= findIntersectingObjects(objectsToTest)
+
+    for( const objects of objectsToTest){
+        objects.material.color.set("red")
+    }
+
+    for( const intersect of intersectingObjects){
+        intersect.object.material.color.set("blue")
+    }
+    // console.log(intersectingObjects)
     // Update controls
     controls.update()
 
