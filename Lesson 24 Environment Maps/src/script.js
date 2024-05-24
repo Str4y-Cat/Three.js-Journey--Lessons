270310ccbb6e43108ba67dab6cd445e9a0731a58
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-05-24 17:00:20 +0200

    used the skybox class to place our object on the 'floor' of the environment map

--- Lesson 24 Environment Maps/src/script.js
+++ Lesson 24 Environment Maps/src/script.js
@@ -4,7 +4,7 @@ import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
 import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js'
-
+import { GroundedSkybox } from 'three/addons/objects/GroundedSkybox.js'
 /**
  * Loaders
  */
@@ -35,7 +35,7 @@ const scene = new THREE.Scene()
 /**
  * Environment Map
  */
-// scene.environmentIntensity=4
+scene.environmentIntensity=1
 // scene.backgroundBlurriness=0
 // scene.backgroundIntensity=0
 
@@ -81,14 +81,30 @@ gui.add(scene.environmentRotation,'y').min(0).max(Math.PI*2).step(0.001).name('e
 
 
 
-// LDR Environment 
-const environmentMap= textureLoader.load("environmentMaps/blockadesLabsSkybox/anime_art_style_japan_streets_with_cherry_blossom_.jpg")
-environmentMap.mapping=THREE.EquirectangularReflectionMapping
-environmentMap.colorSpace=THREE.SRGBColorSpace
+// // LDR Environment 
+// const environmentMap= textureLoader.load("environmentMaps/blockadesLabsSkybox/anime_art_style_japan_streets_with_cherry_blossom_.jpg")
+// environmentMap.mapping=THREE.EquirectangularReflectionMapping
+// environmentMap.colorSpace=THREE.SRGBColorSpace
 
-scene.background=environmentMap
-scene.environment=environmentMap
-scene.environmentIntensity=4
+// scene.background=environmentMap
+// scene.environment=environmentMap
+// scene.environmentIntensity=4
+
+// ground projected skybox
+rgbeLoader.load(
+    '/environmentMaps/2/2k.hdr',
+    (envMap)=>{
+        envMap.mapping=THREE.EquirectangularReflectionMapping
+        scene.environment=envMap
+
+        //skybox
+        const skybox= new GroundedSkybox(envMap,15,70)
+        skybox.position.y=15
+        scene.add(skybox)
+    }
+)
+
+
 /**
  * Torus Knot
  */
