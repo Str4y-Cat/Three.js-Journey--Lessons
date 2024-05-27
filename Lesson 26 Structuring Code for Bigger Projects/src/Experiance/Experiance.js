96eb3e6244c2f28d284ceb0fd90ba82944048f66
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-05-27 11:27:00 +0200

    Added a "experiance" class, which acts as the main class.

---
+++ Lesson 26 Structuring Code for Bigger Projects/src/Experiance/Experiance.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three'
+
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+import Camera from './Camera'
+
+//convert to singleton
+let instance = null
+
+export default class Experience{
+    constructor(canvas){
+        if(instance)
+            {return instance}
+        instance=this
+        
+        console.log('experiance online')
+
+        //global access 
+        window.experience=this
+
+        //options
+        this.canvas=canvas
+
+        //setUp
+        this.sizes=new Sizes()
+        this.time= new Time()
+        this.scene= new THREE.Scene()
+
+        //sizes resize event
+        this.sizes.on('resize',()=>{
+            this.resize()
+        })
+
+        //Time tick event
+        this.time.on('tick',()=>
+            {
+                this.update()
+            })
+    }
+
+    resize(){
+        console.log("resizing")
+    }
+
+    update(){
+        // console.log('update the experiance')
+    }
+}
