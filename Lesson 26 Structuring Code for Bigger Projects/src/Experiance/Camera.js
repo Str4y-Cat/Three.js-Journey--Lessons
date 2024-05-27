03747ce870b65ee20752703387424fb61062e400
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-05-27 11:49:48 +0200

    Finished "camera" class, handles camera and updates where needed. Updates are called from experience class

--- Lesson 26 Structuring Code for Bigger Projects/src/Experiance/Camera.js
+++ Lesson 26 Structuring Code for Bigger Projects/src/Experiance/Camera.js
@@ -1,7 +1,46 @@
+import * as THREE from 'three'
+import Experience from "./Experiance"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 export default class Camera
 {
 
     constructor(){
+        this.experience= new Experience()
+        this.sizes= this.experience.sizes
+        this.scene= this.experience.scene
+        this.canvas= this.experience.canvas
+
         console.log("camera")
+
+        this.setInstance()
+        this.setOrbitControls()
+    }
+
+    setInstance()
+    {
+        this.instance=new THREE.PerspectiveCamera(
+            35,
+            this.sizes.width/this.sizes.height,
+            0.1,
+            100
+        )
+    }
+
+    setOrbitControls()
+    {
+        this.controls= new OrbitControls(this.instance, this.canvas)
+        this.controls.enableDamping=true
+    }
+
+    resize()
+    {
+        this.instance.aspect= this.sizes.width/this.sizes.height
+        this.instance.updateProjectionMatrix()
+    }
+
+    update()
+    {
+        this.controls.update()
     }
 }
