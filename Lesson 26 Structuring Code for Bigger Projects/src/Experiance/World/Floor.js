63568941086b385bec4abfa3561c2b144f5214b2
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-05-28 16:11:02 +0200

    added console logs looking for missing shadow bug

--- Lesson 26 Structuring Code for Bigger Projects/src/Experiance/World/Floor.js
+++ Lesson 26 Structuring Code for Bigger Projects/src/Experiance/World/Floor.js
@@ -58,6 +58,9 @@ export default class Floor
         this.mesh=new THREE.Mesh(this.geometry,this.material)
         this.mesh.rotation.x=-Math.PI *0.5
         this.mesh.recieveShadow=true
+        console.log(this.mesh)
+        console.log(this.mesh.recieveShadow)
+
         this.scene.add(this.mesh)
     }
 
