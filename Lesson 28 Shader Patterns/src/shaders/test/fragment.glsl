517ab2534cf96d815b589e42b93a6f448fbd7bae
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-01 21:06:53 +0200

    Pattern 6. sharp increase gradient

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -9,7 +9,11 @@ void main()
     // float strength= vUv.y
 
     //pattern 5 vertical gradient reversed
-    float strength= 1.0-vUv.y;
+    // float strength= 1.0-vUv.y;
+
+   //pattern 6 vertican gradient shifted 
+   float shiftValue=10.0;
+    float strength= vUv.y*shiftValue;
 
 
     gl_FragColor = vec4(vec3(strength), 1.0);
