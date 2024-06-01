514c41216d280bc14a2445abdea259b8cb17d5ff
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-01 21:26:25 +0200

    pattern 10 horizonatal bars

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -22,8 +22,12 @@ void main()
     // float strength= mod(vUv.y*10.0,1.0);
     // strength= step(0.5, strength);
 
-    //pattern 9 repeated horizontal gradient
-    float strength= mod(vUv.y*10.0,1.0);
+    // //pattern 9 repeated horizontal gradient
+    // float strength= mod(vUv.y*10.0,1.0);
+    // strength= step(0.8, strength);
+
+    //pattern 10 repeated horizontal gradient
+    float strength= mod(vUv.x*10.0,1.0);
     strength= step(0.8, strength);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
