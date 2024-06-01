66af5ff8fd82ed6ef57240c59a01b2d6e3a59420
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-01 20:57:59 +0200

    Pattern 5 reversed vertical gradient

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -2,12 +2,14 @@ varying vec2 vUv;
 
 void main()
 {
-    // pattern 3 white gradient left to right
+    // pattern 3 horizontal gradient
     // float strength=vUv.x;
 
-    //pattern 4 white gradient down to up
-    float strength= vUv.y
+    //pattern 4 vertical gradient
+    // float strength= vUv.y
 
+    //pattern 5 vertical gradient reversed
+    float strength= 1.0-vUv.y;
 
 
     gl_FragColor = vec4(vec3(strength), 1.0);
