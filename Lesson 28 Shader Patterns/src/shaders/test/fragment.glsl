9bfeb36bc6c0a078a49ff872ec7b559b9244661a
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-01 21:25:25 +0200

    pattern 9

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -18,10 +18,13 @@ void main()
     //pattern 7 repeated horizontal gradient
     //float strength= mod(vUv.y*10.0,1.0);
 
-    //pattern 7 repeated horizontal gradient
-    float strength= mod(vUv.y*10.0,1.0);
+    //pattern 8 repeated horizontal gradient
+    // float strength= mod(vUv.y*10.0,1.0);
+    // strength= step(0.5, strength);
 
-    strength= step(0.5, strength);
+    //pattern 9 repeated horizontal gradient
+    float strength= mod(vUv.y*10.0,1.0);
+    strength= step(0.8, strength);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 
