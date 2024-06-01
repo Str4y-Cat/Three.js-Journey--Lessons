93dda92b605a15ab78eb87f22068e2f443253388
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-01 20:55:41 +0200

    pattern 4

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -2,8 +2,12 @@ varying vec2 vUv;
 
 void main()
 {
-    
-    float strength=vUv.x;
+    // pattern 3 white gradient left to right
+    // float strength=vUv.x;
+
+    //pattern 4 white gradient down to up
+    float strength= vUv.y
+
 
 
     gl_FragColor = vec4(vec3(strength), 1.0);
