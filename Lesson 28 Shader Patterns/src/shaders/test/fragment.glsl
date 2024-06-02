ede55d9138552fb587693671677f5fbf10c10e7b
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-02 10:12:48 +0200

    pattern 28

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -113,7 +113,10 @@ void main()
 
 
     //pattern 27
-    float strength= distance(vUv, vec2(0.5));
+    // float strength= distance(vUv, vec2(0.5));
+
+     //pattern 28
+    float strength= 1.0- distance(vUv, vec2(0.5));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 
