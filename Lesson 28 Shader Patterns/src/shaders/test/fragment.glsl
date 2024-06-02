25f7016dc0cb70e263581bb01abfb2de445e2d63
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-02 11:01:45 +0200

    Pattern 32 rotated star

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -1,3 +1,6 @@
+#define PI 3.14159265358979323846264338327950288419
+
+
 varying vec2 vUv;
 
 float random(vec2 st)
@@ -5,8 +8,17 @@ float random(vec2 st)
     return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
 }
 
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
 void main()
 {
+
     // pattern 3 horizontal gradient
     // float strength=vUv.x;
 
@@ -128,19 +140,33 @@ void main()
     // ;
     // float strength= 0.015/distance(lightUv, vec2(0.5));
     
-    //pattern 31
+    // //pattern 31
+    // vec2 lightUv1= vec2(
+    //     vUv.x,
+    //     (vUv.y-0.5)*5.0+0.5)
+    // ;
+    // vec2 lightUv2= vec2(
+    //     (vUv.x-0.5)*5.0+0.5,
+    //     vUv.y)
+    // ;
+    // float strength= 0.15/distance(lightUv1, vec2(0.5));
+    //  strength*= 0.15/distance(lightUv2, vec2(0.5));
+    
+    
+    //pattern 32
+
+    vec2 rotatedUv= rotate(vUv, PI/4.0, vec2(0.5));
+
     vec2 lightUv1= vec2(
-        vUv.x,
-        (vUv.y-0.5)*5.0+0.5)
+        rotatedUv.x,
+        (rotatedUv.y-0.5)*5.0+0.5)
     ;
     vec2 lightUv2= vec2(
-        (vUv.x-0.5)*5.0+0.5,
-        vUv.y)
+        (rotatedUv.x-0.5)*5.0+0.5,
+        rotatedUv.y)
     ;
     float strength= 0.15/distance(lightUv1, vec2(0.5));
      strength*= 0.15/distance(lightUv2, vec2(0.5));
-    
-    
     
     
     gl_FragColor = vec4(vec3(strength), 1.0);
