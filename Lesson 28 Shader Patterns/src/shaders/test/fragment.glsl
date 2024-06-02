7fd19488420d614d4042dbc797606be77a6c733f
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-02 09:36:51 +0200

    pattern 22 checkered gradient

--- Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
+++ Lesson 28 Shader Patterns/src/shaders/test/fragment.glsl
@@ -79,7 +79,10 @@ void main()
     // float strength= square1* square2;
 
     // pattern 21
-    float strength = floor(vUv.x*10.0)/10.0;
+    // float strength = floor(vUv.y*10.0)/10.0;
+
+    // pattern 22
+    float strength = floor(vUv.y*10.0)/10.0*floor(vUv.x*10.0)/10.0;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 
