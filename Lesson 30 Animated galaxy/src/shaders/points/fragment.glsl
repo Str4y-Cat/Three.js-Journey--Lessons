94596a9afa99b8ad850878dc890153366c50dfc5
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-03 20:04:10 +0200

    custom diffuse circle particle

--- Lesson 30 Animated galaxy/src/shaders/points/fragment.glsl
+++ Lesson 30 Animated galaxy/src/shaders/points/fragment.glsl
@@ -5,8 +5,12 @@ varying float vCameraDistance;
 
 void main()
 {
-    // alpha mask
-    float strength= step(0.5,distance(gl_PointCoord,vec2(0.5)));
+    // circle
+    // float strength= step(0.5,distance(gl_PointCoord,vec2(0.5)));
+    // float alpha= 1.0-strength;
+
+    // diffuse circle
+    float strength= distance(gl_PointCoord,vec2(0.5))*2.0;
     float alpha= 1.0-strength;
 
     vec3 myColor= vec3(0.5,0.4,0.9);
