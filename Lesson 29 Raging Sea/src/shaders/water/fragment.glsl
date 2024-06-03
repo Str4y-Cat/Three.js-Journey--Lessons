c9b8cb11ba231fde7bb0b36610fe0b082b223e54
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-03 07:52:49 +0200

    added color

--- Lesson 29 Raging Sea/src/shaders/water/fragment.glsl
+++ Lesson 29 Raging Sea/src/shaders/water/fragment.glsl
@@ -1,5 +1,7 @@
 uniform vec3 uBigWavesColor1;
 uniform vec3 uBigWavesColor2;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
 
 
 
@@ -8,7 +10,8 @@ varying float vDepth;
 
 void main()
 {
-    vec3 waveColor= mix(uBigWavesColor1,uBigWavesColor2,vDepth+1.0);
+    float mixStrength= (vDepth+uColorOffset)*uColorMultiplier;
+    vec3 waveColor= mix(uBigWavesColor1,uBigWavesColor2,mixStrength);
     // float color= step(0.5,vDepth);
     // vec3 waveColor= vec3(color);
     gl_FragColor= vec4(waveColor, 1.0);
