3adc84db4d411288c35c2ea9c4cfeab34b45a731
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-09-01 08:35:10 +0200

    Lesson 48 init

--- Lesson 48 Mixing WebGL and HTML/src/script.js
+++ Lesson 48 Mixing WebGL and HTML/src/script.js
@@ -1,53 +1,38 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import fragmentShader from "./shaders/intro/fragment.glsl"
-import vertexShader from "./shaders/intro/vertex.glsl"
-import gsap from 'gsap'
+import { gsap } from 'gsap'
 
 /**
  * Loaders
  */
-const bar = document.querySelector(".loading-bar")
+const loadingBarElement = document.querySelector('.loading-bar')
+const loadingManager = new THREE.LoadingManager(
+    // Loaded
+    () =>
+    {
+        // Wait a little
+        window.setTimeout(() =>
+        {
+            // Animate overlay
+            gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0, delay: 1 })
 
-const loadingManager= new THREE.LoadingManager(
-    
-        //loaded
-        ()=>
-            {
-                window.setTimeout(()=>{},500)
-                gsap.to(overlayMateral.uniforms.uAlpha,
-                    {
-                        value:0,
-                        duration:3,
+            // Update loadingBarElement
+            loadingBarElement.classList.add('ended')
+            loadingBarElement.style.transform = ''
+        }, 500)
+    },
 
-                    }
-                )
-                bar.classList.add('ended')
-                bar.style.transform=``
-
-                // gsap.to(bar,
-                //     {
-                //         opacity:0,
-                //         duration:2,
-
-                //     }
-                // )
-                console.log('loaded')
-
-            },
-        //Progress
-        (url,loaded,total)=>
-            {   
-                console.log(url)
-                bar.style.transform=`scaleX(${loaded/total})`
-                // console.log(loaded)
-
-            }
-    
+    // Progress
+    (itemUrl, itemsLoaded, itemsTotal) =>
+    {
+        // Calculate the progress and update the loadingBarElement
+        const progressRatio = itemsLoaded / itemsTotal
+        loadingBarElement.style.transform = `scaleX(${progressRatio})`
+    }
 )
 const gltfLoader = new GLTFLoader(loadingManager)
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
@@ -61,19 +46,33 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-//overlay
-const overlayGeometry= new THREE.PlaneGeometry(2,2,1,1)
-const overlayMateral= new THREE.ShaderMaterial(
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    // wireframe: true,
+    transparent: true,
+    uniforms:
     {
-        fragmentShader:fragmentShader,
-        vertexShader:vertexShader,
-        transparent:true,
-        uniforms:
+        uAlpha: { value: 1 }
+    },
+    vertexShader: `
+        void main()
         {
-            uAlpha:new THREE.Uniform(1)
+            gl_Position = vec4(position, 1.0);
         }
-    })
-const overlay= new THREE.Mesh(overlayGeometry,overlayMateral)
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+
+        void main()
+        {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
 scene.add(overlay)
 
 /**
@@ -117,11 +116,10 @@ debugObject.envMapIntensity = 2.5
  * Models
  */
 gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
     (gltf) =>
     {
-        gltf.scene.scale.set(10, 10, 10)
-        gltf.scene.position.set(0, - 4, 0)
+        gltf.scene.scale.set(2.5, 2.5, 2.5)
         gltf.scene.rotation.y = Math.PI * 0.5
         scene.add(gltf.scene)
 
